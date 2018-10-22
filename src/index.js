fed624ea450bae06eca8e3009fa7ad505fce5bb4
ya_ron <dr.likle@gmail.com>
2018-10-23 02:02:49 +0300

    Test

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const books = {
+    'Jewels of Nizam': 'Geeta Devi',
+    'Cakes & Bakes': 'Sanjeev Kapoor',
+    'Jamie\'s Kitchen': 'Jamie Oliver',
+    'Inexpensive Family Meals': 'Simon Holst',
+    'Paleo Slow Cooking':'Chrissy Gower',
+    'Cook Like an Italian': 'Tobie Puttock',
+    'Suneeta Vaswani': 'Geeta Devi',
+    'Jamie Does': 'Jamie Oliver',
+    'Jamie\'s italy': 'Jamie Oliver',
+    'Vegetables Cookbook':'Matthew Biggs'
+};
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+    mode: 'development',
+    entry: ['./src/index.js', './src/scss/index.scss'],
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename:'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options:  {
+                                sourceMap: true,
+                                url: false,
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true,
+                            }
+                        }
+                    ]
+                }),
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: './src/index.html'
+            }
+        ]),
+        new CopyWebpackPlugin([
+            {
+                from: './src/index.js'
+            }
+        ]),
+        new CopyWebpackPlugin([
+            {
+                from: './src/fonts',
+                to: 'fonts'
+            }
+        ]),
+        new CopyWebpackPlugin([
+            {
+                from: './src/images',
+                to: 'images'
+            }
+        ]),
+        new ExtractTextPlugin({
+            filename: 'style.css',
+            allChunks: true
+        })
+    ]
+};
